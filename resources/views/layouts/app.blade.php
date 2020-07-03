86452f6b052b0aff14f2535edfe2100e1f640917
Infectos <infectos@yandex.ru>
2020-07-03 19:18:23 +0300

    admin panel added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,10 +55,16 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    
+                                    @if (Auth::user()->isAdmin)
+                                    <a class="dropdown-item" href="/admin">
+                                        {{ __('Кабинет Админа') }}
+                                    </a>
+                                    @endif
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('Выйти') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
